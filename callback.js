d2802e110dd8c677e886e3c9f6305911abc6ed35
niteshv10021997 <146006984+niteshv10021997@users.noreply.github.com>
2026-04-06 11:49:15 +0530

    Implement createPosts and getPosts functions
    Added functionality to create and retrieve posts asynchronously.

--- callback.js
+++ callback.js
@@ -1,1 +1,25 @@
 
+const posts = [
+    {title: "First Post", description: "This is my first post"},
+    {title: "Second Post", description: "This is my second post"}
+]
+
+const createPosts = (post, getPostsCallback) => {
+    setTimeout(() => {
+        posts.push(post);
+        getPostsCallback();
+    }, 2000)
+}
+
+const getPosts = () => {
+    setTimeout(() => {
+        let output = "";
+        posts.forEach(post => {
+           output += `<li>${post.title}</li>`
+        })
+        document.body.innerHTML = output;
+    }, 1000)
+}
+
+createPosts({title: "Third Post", description: "This is my third post"}, getPosts);
+getPosts();
